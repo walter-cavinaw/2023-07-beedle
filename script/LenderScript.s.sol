f12e4ec08b0913edb5723296189a4e506ede6209
Walter Cavinaw <w.cavinaw@gmail.com>
2024-08-25 20:36:27 -0700

    save changes.

--- script/LenderScript.s.sol
+++ script/LenderScript.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../src/Lender.sol";
 import "../src/Staking.sol";
-import "../src/Beedle.sol";
+import "../src/Blee.sol";
 
 import {WETH} from "solady/src/tokens/WETH.sol";
 import {VestingWallet} from "openzeppelin-contracts/contracts/finance/VestingWallet.sol";
@@ -14,9 +14,9 @@ contract LenderScript is Script {
         uint256 deployer = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployer);
         new Lender();
-        address beedle = address(new Beedle());
+        address blee = address(new Blee());
         address weth = address(new WETH());
-        address staking = address(new Staking(beedle, weth));
+        address staking = address(new Staking(blee, weth));
 
         vm.stopBroadcast();
     }
--- src/Blee.sol
+++ src/Blee.sol
@@ -6,9 +6,9 @@ import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import {ERC20Votes} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract Beedle is Ownable, ERC20, ERC20Permit, ERC20Votes {
+contract Blee is Ownable, ERC20, ERC20Permit, ERC20Votes {
 
-    constructor() ERC20("Beedle", "BDL") ERC20Permit("Beedle") Ownable(msg.sender) {
+    constructor() ERC20("Blee", "BDL") ERC20Permit("Blee") Ownable(msg.sender) {
         _mint(msg.sender, 1_000_000_000 * 1e18);
     }
 
